73f8b628e7b708eaa787c42b04b76c2653509e42
insamdev <zoony82@gmail.com>
2020-05-24 02:23:12 +0900

    interface/class test

--- src/hello2.ts
+++ src/hello2.ts
@@ -1,12 +1,32 @@
-function greeter(person:any) {
-    return "Hello, " + person;
+class Student {
+    fullName : string;
+    constructor(public firstName : string, public publicName : string, public lastName : string){
+        this.fullName = firstName + " " + publicName + " " + lastName
+    }
 }
 
+interface Person {
+    firstName : string;
+    lastName : string;
+}
 
-let user = "insamdev-1";
-const result = greeter(user);
+function greeter(person:Person) {
+    return "Hello, " + person.lastName + " " + person.firstName;
+}
 
-console.log(result)
+var user = {firstName:"준희", lastName:"장"}
+document.body.innerHTML = greeter(user);
+
+var student = new Student("준희","인삼", "장")
+document.body.innerHTML =  greeter(student);
+
+// open hello2.html
+
+// console.log(result)
+// document.body.textContent = result
+// document.body.innerHTML = result
 
 // tsc
 // node dist/hello2.js
+
+// open hello2.html
